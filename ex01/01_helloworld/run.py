5682a48ebe442acc92315d9465390489338867dc
Liam <debevecliam@gmail.com>
2024-04-24 11:25:33 -0400

    exercise 1, result not working

---
+++ ex01/01_helloworld/run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import bottle
+
+@bottle.route('/')
+def index():
+	return bottle.template('index')
+@bottle.route('/<name>')
+def message(name):
+    the_message = f'Hello {name}!'
+    return bottle.template('template', message=the_message)
+
+bottle.run(host='0.0.0.0', port=8090)
